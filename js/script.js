07feae9329d2452a7ac3f9e028fef56920c3531d
Simran Rai <raicalvin@gmail.com>
2023-12-19 21:16:39 -0800

    add basic navigation menu open and close functionality and code cleanup

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 function openExternalUrl(url) {
     window.open(url)
 }
+
+function toggleNavigationMenu() {
+    var navMenu = document.getElementById("navmenu");
+    navMenu.classList.toggle("navigation-inactive");
+
+    var mobileNavMenu = document.getElementById("navmenumobile");
+    mobileNavMenu.classList.toggle("navigation-mobile-active");
+}
