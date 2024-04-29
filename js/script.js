c2b80280ff5866aa2363a4358a7f4a83a6276420
Calvin Rai <raicalvin@gmail.com>
2024-04-28 20:41:11 -0700

    [v0.17] Page for Pavi's Treasure Chest Scavenger Hunt (#21)

--- js/script.js
+++ js/script.js
@@ -13,3 +13,18 @@ function toggleNavigationMenu() {
 function downloadResume() {
     window.open("docs/crai_resume_public.pdf")
 }
+
+function paviPooClick() {
+    const opacityLevel = 0.25
+    document.getElementById("paviTreasureChest").src = "../img/pavipoo/treasure.jpeg";
+    document.getElementById("paviOpacity1").style.opacity = opacityLevel;
+    document.getElementById("paviOpacity2").style.opacity = opacityLevel;
+    document.getElementById("paviOpacity3").style.opacity = opacityLevel;
+    document.getElementById("paviOpacity4").style.opacity = opacityLevel;
+    document.getElementById("paviOpacity5").style.opacity = opacityLevel;
+    document.getElementById("paviOpacity6").style.opacity = opacityLevel;
+    document.getElementById("paviOpacity8").style.opacity = opacityLevel;
+    document.getElementById("paviOpacity9").style.opacity = opacityLevel;
+    document.getElementById("paviOpacity10").style.opacity = opacityLevel;
+
+}
